0763c3d122428ce931a4ff8d57ff281c315aa235
JCamp-12 <username@users.noreply.github.com>
2017-11-13 20:29:39 -0500

    delete working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {Appbar, Container} from 'muicss/react';
+import {Appbar, Container, Button} from 'muicss/react';
 import './App.css';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
@@ -72,6 +72,29 @@ addTask(text){
   });
 }
 
+clearTasks(){
+  let tasks = this.state.tasks;
+  let i = tasks.length;
+
+  while(i--) {
+    if(tasks[i].completed === true) {
+      let id = tasks[i]._id.$oid;
+      tasks.splice(i, 1);
+      axios.request({
+        method: 'delete',
+        url: 'https://api.mlab.com/api/1/databases/lambdatasks/collections/Tasks/'+id+'?apiKey=l6_6r7-Qq_oYkMTA5Brl_UtmbcPci3P9'
+      }).then((response) => {
+
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+  }
+
+  this.setState({tasks: tasks});
+}
+
+
   render() {
     return (
       <div className="App">
@@ -79,7 +102,7 @@ addTask(text){
         <table width="100%">
          <tbody>
            <tr>
-             <td className="mui--appbar-height"><h2>LambdaTasks</h2></td>
+             <td className="mui--appbar-height"><h2>Get Your Tasks Here</h2></td>
            </tr>
          </tbody>
        </table>
@@ -88,6 +111,7 @@ addTask(text){
         <Container>
           <AddTask onAddTask={this.addTask.bind(this)} />
           <Tasks onEditState={this.editState.bind(this)} tasks={this.state.tasks} />
+          <Button color="danger" onClick={this.clearTasks.bind(this)}>Clear Task</Button>
         </Container>
       </div>
     );
